8ed4a16a2245df4d74d711240811668500b687ef
Audrius Karosevicius <audrius.karosevicius@gmail.com>
2018-07-16 15:23:58 +0300

    Removing GET_ACCOUNTS permission

--- app/src/main/java/net/kwatts/powtools/util/Util.java
+++ app/src/main/java/net/kwatts/powtools/util/Util.java
@@ -1,49 +1,24 @@
 package net.kwatts.powtools.util;
 
-import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.content.Context;
 import android.support.annotation.IntRange;
 
 import java.util.BitSet;
-import java.util.LinkedList;
-import java.util.List;
+
 /**
  * Created by kwatts on 6/15/16.
  */
 public class Util {
-
-
-    public static String getUsername(Context c) {
-        AccountManager manager = AccountManager.get(c);
-        Account[] accounts = manager.getAccountsByType("com.google");
-        List<String> possibleEmails = new LinkedList<String>();
-
-        for (Account account : accounts) {
-            // TODO: Check possibleEmail against an email regex or treat
-            // account.name as an email address only for certain account.type values.
-            possibleEmails.add(account.name);
-        }
-
-        if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
-            String email = possibleEmails.get(0);
-            String[] parts = email.split("@");
-
-            if (parts.length > 1)
-                return parts[0];
-        }
-        return null;
-    }
 
     /* Helper methods. For dealing with bytes see:
        http://www.roseindia.net/java/master-java/bitwise-bitshift-operators.shtml
        https://calleerlandsson.com/2014/02/06/rubys-bitwise-operators/
      */
     public static short byteToShort(byte[] v) {
-        return (short)((v[1] << 8) + (v[0] & 0xff));
+        return (short) ((v[1] << 8) + (v[0] & 0xff));
     }
 
     private static final byte[] twoBytes = new byte[2];
+
     public static byte[] intToShortBytes(@IntRange(from = -32768, to = 32767) int v) {
 
         twoBytes[0] = (byte) (v >> 8 & 0xff);
@@ -51,6 +26,7 @@ public class Util {
 
         return twoBytes;
     }
+
     public static byte[] boolToShortBytes(boolean b) {
         return new byte[0];
     }
@@ -64,7 +40,7 @@ public class Util {
     // short = 2 bytes/16-bit signed two's complement (-32,768 to 32,767)
     public static int unsignedShort(byte[] var0) {
         int var1;
-        if(var0.length < 2) {
+        if (var0.length < 2) {
             var1 = -1;
         } else {
             var1 = (unsignedByte(var0[0]) << 8) + unsignedByte(var0[1]);
@@ -81,31 +57,31 @@ public class Util {
 
     // double is 64 bit and used for decimals
     public static double cel2far(int celsius) {
-        return (9.0/5.0)*celsius + 32;
+        return (9.0 / 5.0) * celsius + 32;
     }
+
     public static double far2cel(int far) {
-        return (5.0/9.0)*(far - 32);
+        return (5.0 / 9.0) * (far - 32);
     }
+
     public static double milesToKilometers(double paramDouble) {
         return paramDouble * 1.609344;
 
     }
-    public static double revolutionsToKilometers(double paramDouble)
-    {
+
+    public static double revolutionsToKilometers(double paramDouble) {
         return paramDouble * 35.0D / 39370.099999999999D;
     }
 
-    public static double revolutionsToMiles(double paramDouble)
-    {
+    public static double revolutionsToMiles(double paramDouble) {
         return paramDouble * 35.0D / 63360.0D;
     }
-    public static double rpmToKilometersPerHour(double paramDouble)
-    {
+
+    public static double rpmToKilometersPerHour(double paramDouble) {
         return 60.0D * (35.0D * paramDouble) / 39370.099999999999D;
     }
 
-    public static double rpmToMilesPerHour(double paramDouble)
-    {
+    public static double rpmToMilesPerHour(double paramDouble) {
         return 60.0D * (35.0D * paramDouble) / 63360.0D;
     }
 
@@ -127,23 +103,24 @@ public class Util {
 
     /**
      * If your number X falls between A and B, and you would like Y to fall between C and D, you can apply the following linear transform:
-     *
+     * <p>
      * Y = (X-A)/(B-A) * (D-C) + C
-     *
+     * <p>
      * https://stackoverflow.com/a/345204/247325
      */
     public static float linearTransform(float x, float a, float b, float c, float d) {
-        return (x-a)/(b-a) * (d-c) + c;
+        return (x - a) / (b - a) * (d - c) + c;
     }
+
     /**
      * If your number X falls between A and B, and you would like Y to fall between C and D, you can apply the following linear transform:
-     *
+     * <p>
      * Y = (X-A)/(B-A) * (D-C) + C
-     *
+     * <p>
      * https://stackoverflow.com/a/345204/247325
      */
     public static double linearTransform(double x, double a, double b, double c, double d) {
-        return (x-a)/(b-a) * (d-c) + c;
+        return (x - a) / (b - a) * (d - c) + c;
     }
 
     public static double round(double value, int precision) {
